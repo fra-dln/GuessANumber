820298598e73937b2acc15d5ce06ef44460d273b
kecco9939 <kecco9939@gmail.com>
2021-04-19 12:38:51 +0200

    modify layout

--- app/src/main/java/it/francescodalena/guessanumber/MainActivity.kt
+++ app/src/main/java/it/francescodalena/guessanumber/MainActivity.kt
@@ -2,10 +2,17 @@ package it.francescodalena.guessanumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import it.francescodalena.guessanumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
